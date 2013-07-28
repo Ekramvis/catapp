d674bcf9a5fe5b26e2cefc55c273d2e81a04b997
Ekramvis <ekramvis@gmail.com>
2013-07-28 10:42:06 -0700

    Update cat.rb

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -11,13 +11,13 @@ class Cat < ActiveRecord::Base
   end
 
   def age_in_completed_years (bd, d)
-      # Difference in years, less one if you have not had a birthday this year.
-      a = d.year - bd.year
-      a = a - 1 if (
-           bd.month >  d.month or
-          (bd.month >= d.month and bd.day > d.day)
-      )
-      a
+    # Difference in years, less one if you have not had a birthday this year.
+    a = d.year - bd.year
+    a = a - 1 if (
+      bd.month >  d.month or
+      (bd.month >= d.month and bd.day > d.day)
+    )
+    a
   end
 end
 
